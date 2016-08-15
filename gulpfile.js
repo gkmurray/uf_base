8aa707af9f29c896d28db85c50e3c2eb3e1aa65d
Greg <gkmurray@live.com>
2016-08-15 16:20:13 -0300

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//Include gulp
+var gulp = require('gulp');
+
+//defs
+var sassPaths = [
+	'src/sass',
+	'bower_components/foundation-sites/scss',
+	'bower_components/motion-ui/src'
+];
+
+//Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+//Compile and minify SASS
+gulp.task('sass', function() {
+	return gulp.src('src/sass/style.scss')
+		.pipe(sass({
+			includePaths: sassPaths,
+			outputStyle: 'compressed' // if css compressed **file size**
+			
+		}))
+		.pipe(prefix({
+		  browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
+		}))
+		.pipe(gulp.dest('./'));
+});
+
+//Concatenate all JS files, then minify
+gulp.task('scripts', function() {
+	return gulp.src('src/js/*.js')
+		.pipe(concat('main.js'))
+			.pipe(rename({suffix: '.min'}))
+			.pipe(uglify())
+			.pipe(gulp.dest('dist/js'));
+});
+
+//Default Task
+gulp.task('default', ['sass']);
