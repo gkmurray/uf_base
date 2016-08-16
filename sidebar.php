191f54a37379019e28096a04457eec83f2132af1
Shaun <shaun@analytic-or.com>
2016-08-16 14:07:59 -0300

    Update base layout with Foundation classes

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+<div class="column medium-4" >
+	<aside id="secondary" class="widget-area" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</aside><!-- #secondary -->
+</div><!-- .column -->
+</div><!-- .row -->
