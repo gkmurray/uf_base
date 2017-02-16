b1be3fc244724b365d34ff9b735c9ba0fa20d77b
Shaun <readysd@gmail.com>
2017-02-16 16:51:15 -0400

    Remove duplicate comment sections

--- parts/loop-single.php
+++ parts/loop-single.php
@@ -13,8 +13,6 @@
 	<footer class="article-footer">
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jointswp' ), 'after'  => '</div>' ) ); ?>
 		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>	
-	</footer> <!-- end article footer -->
-						
-	<?php comments_template(); ?>	
+	</footer> <!-- end article footer -->	
 													
 </article> <!-- end article -->
